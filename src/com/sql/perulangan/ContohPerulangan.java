945fe208331e9e54b1cd8938d6f24915b296a0ba
Koswara <koswara.c79@gmail.com>
2021-11-24 10:26:38 +0700

    first commit

---
+++ src/com/sql/perulangan/ContohPerulangan.java
@@ -0,0 +1,11 @@
+package com.sql.perulangan;
+
+public class ContohPerulangan {
+	public static void main(String[] args) {
+		//Aplikasi Raraban
+		int N = 8;
+		for(int i=1;i<=20;i++) {
+			System.out.printf("%d x %d = %d%n", N, i, N*i);
+		}
+	}
+}
